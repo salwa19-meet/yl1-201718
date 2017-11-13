69b36ff17d3191fcc69c9ea3e9d4ca59a16ae7ce
shiraz19-meet <shiraz19@meet.mit.edu>
2017-11-13 18:38:13 +0200

    uploaded my labs

---
+++ lab3/TurtleShapes.py
@@ -0,0 +1,28 @@
+import turtle
+#turtle.pendown()
+#turtle.pencolor("blue")
+#turtle.goto(-100,-100)
+#turtle.pencolor("red")
+#turtle.goto(0,150)
+#turtle.pencolor("green")
+#turtle.goto(100,-100)
+#turtle.pencolor("pink")
+#turtle.goto(-100,100)
+#turtle.pencolor("purple")
+#turtle.goto(100,100)
+#turtle.pencolor("yellow")
+#turtle.goto(0,0)
+
+#turtle.register_shape("salwa",((50,0),(0,50),(0,100),(100,100),(100,50),(50,0)))
+#turtle.shape("salwa")
+
+
+import turtle
+turtle.penup()
+turtle.goto(0,50)
+turtle.pendown()
+turtle.goto(100,50)
+turtle.goto(150,25)
+turtle.goto(100,0)
+turtle.goto(0,50)
+turtle.mainloop()
