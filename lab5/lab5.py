748f3e6be79fd6c9a6873ac7bcd11fc4d5740bcc
shiraz19-meet <shiraz19@meet.mit.edu>
2017-11-27 18:27:52 +0200

    added lab5

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,4 +1,6 @@
-from turtle import Turtle, colormode
+from turtle import *
+import random 
+colormode(255)
 
 class Square(Turtle):
 	def __init__(self,size):
@@ -13,8 +15,25 @@ class Square(Turtle):
 		self.color(r, g, b)
 	
 square1 = Square(30)
-square1.random_color(r,g,b)
+square1.random_color()
+#########################
 
 
 
-		
+class Hexagon(Turtle):
+	def __init__(self,size,color):
+		Turtle.__init__(self)
+		self.begin_poly()
+		self.color(color)
+		self.speed('slow')
+		self.begin_fill()
+		for i in range(6):
+
+			self.forward(size)				
+			self.right(360/6)
+		self.end_fill()
+		self.end_poly()
+	 	register_shape("Hex", self.get_poly())
+	 	
+
+hexagon = Hexagon(50,"red")
